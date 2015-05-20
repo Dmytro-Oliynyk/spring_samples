7f8db39d095ddde271be682549ebc2163b5f826e
Dmytro-Oliynyk <mefistofel902@gmail.com>
2015-05-20 18:27:13 +0300

    addad log4j

--- xml-config/src/test/java/com/dmytro/pdp/spring/controler/UserManagerTest.java
+++ xml-config/src/test/java/com/dmytro/pdp/spring/controler/UserManagerTest.java
@@ -1,24 +1,27 @@
 package com.dmytro.pdp.spring.controler;
 
 import com.dmytro.pdp.spring.UserManager;
+
+
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * Created by Dmytro_Olijnyk on 5/18/2015.
  */
 
 public class UserManagerTest {
+     private static final Logger log = Logger.getLogger(UserManagerTest.class);
 
     @Test
     public void xmlDI_And_SpEL() throws Exception {
-        System.out.printf("START");
-
+        log.info("START");
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         UserManager manager = (UserManager) context.getBean("usersSet");
-
-        System.out.println(manager.printInfo());
-        System.out.printf("END");
+        log.info(manager.printInfo());
+        log.info("END");
     }
 }
