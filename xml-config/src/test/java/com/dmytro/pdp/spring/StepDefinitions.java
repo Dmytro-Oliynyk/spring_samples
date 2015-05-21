98977a2eb39091c1a5e24a25173ec892eb429c8f
Dmytro-Oliynyk <mefistofel902@gmail.com>
2015-05-21 16:33:12 +0300

    Added cucumber for BDD tests

---
+++ xml-config/src/test/java/com/dmytro/pdp/spring/StepDefinitions.java
@@ -0,0 +1,42 @@
+package com.dmytro.pdp.spring;
+
+import cucumber.api.java8.En;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Dmytro_Olijnyk on 5/21/2015.
+ */
+public class StepDefinitions implements En {
+    private static final Logger log = Logger.getLogger(UserManagerTest.class);
+    private ApplicationContext context;
+    private UserManager manager;
+
+    public StepDefinitions() {
+        Given("^I init spring context from (.*)$", (String configXml) -> {
+            log.info("UserManagerTest: START");
+            this.context = new ClassPathXmlApplicationContext(configXml);
+        });
+        When("^I get (.*)$", (String beanName) -> {
+            this.manager = (UserManager) context.getBean(beanName);
+            log.info("UserManagerTest: init object");
+        });
+        Then("^My user (.*) (.*) is in line$", (String firstName, String lastName) -> {
+            Stream<User> userStream = this.manager.getUsers().stream().filter(new Predicate<User>() {
+                @Override
+                public boolean test(User user) {
+                    return user.getFirstName().equals(firstName) && user.getLastName().equals(lastName);
+                }
+            });
+            assertNotNull(userStream);
+            log.info(this.manager.printInfo());
+            log.info("UserManagerTest: END");
+        });
+    }
+}
---
+++ xml-config/src/test/resources/com/dmytro/pdp/spring/user.feature
@@ -0,0 +1,6 @@
+Feature: User manager
+
+  Scenario:  User manager flow
+    Given I init spring context from spring-config.xml
+    When I get usersSet
+    Then My user Jon Dou is in line
